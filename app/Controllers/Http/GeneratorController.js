4462987bcdb22eeb42b85d15bfd16d38e8b70686
“BagusAK95” <bagus.ak95@gmail.com>
2019-04-09 16:00:00 +0700

    Generate Input

--- app/Controllers/Http/GeneratorController.js
+++ app/Controllers/Http/GeneratorController.js
@@ -2,11 +2,33 @@
 
 class GeneratorController {
     async generate({ request, response }) {
-
+        const trainingConfig = request.body
+        const result = this.generateTrainingList(trainingConfig)
+        response.send(result)
     }
 
     generateTrainingList(trainingConfig) {
+        const { wordList } = trainingConfig
 
+        const result = [];
+        let strFunc = '';
+        let strJoin = [];
+
+        for (let i = 0; i < wordList.length; i++) {
+            strJoin.push('word' + i)
+        }
+
+        for (let i = wordList.length - 1; i >= 0; i--) {
+            if (i == wordList.length - 1) {
+                strFunc = 'for (const word' + i + ' of wordList[' + i + ']) {\n result.push({ input: ' + strJoin.join(' + " " + ') + ', entities:[] }) \n}'
+            } else {
+                strFunc = 'for (const word' + i + ' of wordList[' + i + ']) {\n' + strFunc + '\n}'
+            }
+        }
+
+        eval(strFunc)
+
+        return result
     }
 
     completeTrainingList() {
